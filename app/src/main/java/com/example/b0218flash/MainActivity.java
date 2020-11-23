7fb3c5c239de6144bbfd81f5b33395adc055b45f
mlapinm <mlapin@rambler.ru>
2020-11-23 21:25:18 +0300

    Initial commit

---
+++ app/src/main/java/com/example/b0218flash/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.b0218flash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.accessibilityservice.AccessibilityService;
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AccessibilityService context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickFlash(View view){
+        CameraManager cameraManager = (CameraManager)getSystemService(Context.CAMERA_SERVICE);
+        try{
+            assert  cameraManager != null;
+            String cameraId = cameraManager.getCameraIdList()[0];
+            cameraManager.setTorchMode(cameraId, true);
+        }catch (CameraAccessException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        CameraManager cameraManager = (CameraManager)getSystemService(Context.CAMERA_SERVICE);
+        try{
+            assert  cameraManager != null;
+            String cameraId = cameraManager.getCameraIdList()[0];
+            cameraManager.setTorchMode(cameraId, false);
+        }catch (CameraAccessException e){
+            e.printStackTrace();
+        }
+    }
+}
