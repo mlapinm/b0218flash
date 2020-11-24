e70b64ef8c4087acb73430f977864e5ad780b652
Mikhail Lapin <mlapin@rambler.ru>
2020-11-24 09:27:58 +0300

    Stop button

--- app/src/main/java/com/example/b0218flash/MainActivity.java
+++ app/src/main/java/com/example/b0218flash/MainActivity.java
@@ -17,9 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void onClickFlash(View view){
         CameraManager cameraManager = (CameraManager)getSystemService(Context.CAMERA_SERVICE);
         try{
             assert  cameraManager != null;
@@ -28,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
         }catch (CameraAccessException e){
             e.printStackTrace();
         }
+    }
+
+    public void onClickFlash(View view){
+        onDestroy();
     }
 
     @Override
